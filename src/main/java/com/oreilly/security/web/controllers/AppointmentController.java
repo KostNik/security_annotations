ae5b3e0b7e129b7aaab895c9653b46546b2ac946
Kostiuk_N <kost.nik88@gmail.com>
2019-02-17 22:21:25 +0200

    sec:taglib

--- src/main/java/com/oreilly/security/web/controllers/AppointmentController.java
+++ src/main/java/com/oreilly/security/web/controllers/AppointmentController.java
@@ -1,60 +1,63 @@
 package com.oreilly.security.web.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.oreilly.security.domain.entities.Appointment;
 import com.oreilly.security.domain.entities.AutoUser;
 import com.oreilly.security.domain.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller()
 @RequestMapping("/appointments")
 public class AppointmentController {
 
-	@Autowired
-	private AppointmentRepository appointmentRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
-	@ModelAttribute
-	public Appointment getAppointment(){
-		return new Appointment();
-	}
+    @ModelAttribute
+    public Appointment getAppointment() {
+        return new Appointment();
+    }
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String getAppointmentPage(){
-		return "appointments";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getAppointmentPage() {
+        return "appointments";
+    }
 
-	@ResponseBody
-	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public List<Appointment> saveAppointment(@ModelAttribute Appointment appointment){
-		AutoUser user= (AutoUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		appointment.setUser(user);
-		appointment.setStatus("Initial");
-		appointmentRepository.save(appointment);
-		return this.appointmentRepository.findAll();
-	}
-	
-	@ResponseBody
-	@RequestMapping("/all")
-	public List<Appointment> getAppointments(){
-		return this.appointmentRepository.findAll();
-	}
+    @ResponseBody
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public List<Appointment> saveAppointment(@ModelAttribute Appointment appointment) {
+        AutoUser user = (AutoUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        appointment.setUser(user);
+        appointment.setStatus("Initial");
+        appointmentRepository.save(appointment);
+        return this.appointmentRepository.findAll();
+    }
 
-	@RequestMapping("/{appointmentId}")
-	public String getAppointment(@PathVariable("appointmentId") Long appointmentId, Model model){
-		Appointment appointment = appointmentRepository.findOne(appointmentId);
-		model.addAttribute("appointment", appointment);
-		return "appointment";
-	}
-	
+    @ModelAttribute("isUser")
+    public boolean isUser(Authentication authentication) {
+        return authentication != null
+                && authentication.getAuthorities().contains(AuthorityUtils.createAuthorityList("ROLE_USER").get(0));
+    }
+
+    @ResponseBody
+    @RequestMapping("/all")
+    public List<Appointment> getAppointments() {
+        return this.appointmentRepository.findAll();
+    }
+
+    @RequestMapping("/{appointmentId}")
+    public String getAppointment(@PathVariable("appointmentId") Long appointmentId, Model model) {
+        Appointment appointment = appointmentRepository.findOne(appointmentId);
+        model.addAttribute("appointment", appointment);
+        return "appointment";
+    }
+
 }
